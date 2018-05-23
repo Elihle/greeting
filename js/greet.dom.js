6a50a3cbd2e30e23168e4ff3d18ae04fd25e3572
Lihle <lihlemenz@gmail.com>
2018-05-23 16:30:17 +0200

    Checked stored names on local storage

--- js/greet.dom.js
+++ js/greet.dom.js
@@ -1,97 +1,63 @@
-  var greetCounterElem = document.querySelector('.greetCounter');
-  var inputName = document.querySelector('.inputName');
-  var greetBtnElement = document.querySelector('.greetBtn');
-  var clearBtnElement = document.querySelector('.clearBtn');
-  var greetingElement = document.querySelector('.greeting');
-  var language = document.querySelector('.language');
+var greetCounterElem = document.querySelector('.greetCounter');
+var inputName = document.querySelector('.inputName');
+var greetBtnElement = document.querySelector('.greetBtn');
+var clearBtnElement = document.querySelector('.clearBtn');
+var greetingElement = document.querySelector('.greeting');
+var language = document.querySelector('.language');
 
-  // var count = 0;
-  // var storedCounter = localStorage.getItem('counted');
-  //
-  // if (storedCounter) {
-  //   count = Number(storedCounter);
-  // }
+//var storedCounter = JSON.parse(localStorage.getItem('counted'));
+//console.log(storedCounter);
+
+// count = 0;
+//
+// // if (storedCounter) {
+// //   count = Number(storedCounter);
+// // }
 
 
-  function getPeopleGreetedFromStorage(){
-      var greetedNames = {};
-    if (localStorage['greetedNames']){
+var checkStoredNames = function (){
+  var storedNames = JSON.parse(localStorage.getItem("greetedNames"));
+  if (storedNames){
+    return storedNames;
+  }else{
+   localStorage.setItem('greetedNames',JSON.stringify({}));
+   return storedNames;
+  }
+}
+var greetSpotter = Greetings(checkStoredNames());
 
-      var retrieved = localStorage.getItem('greetedNames');
-       greetedNames =JSON.parse(retrieved);
-    }
+greetCounterElem.innerHTML = greetSpotter.countAllGreets();
 
-    return greetedNames;
+function clickGreetBtn() {
+  var name = inputName.value;
+  var checkedRadioBtn = document.querySelector("input[name='language']:checked");
+
+  if (checkedRadioBtn) {
+    var languageChoice = checkedRadioBtn.value;
+    var displayGreeting = greetSpotter.allGreetings(languageChoice, name);
+    console.log(displayGreeting);
+
+    var greetNo = greetSpotter.countAllGreets();
   }
 
-  var greetMe  = Greetings(getPeopleGreetedFromStorage());
-  var greetsNo = greetMe.countAllGreets();
-  greetCounterElem.innerHTML = greetsNo;
+  greetingElement.innerHTML = displayGreeting;
+  greetCounterElem.innerHTML =   greetSpotter.countAllGreets();
 
-  function clickGreetBtn(){
+  //localStorage.setItem('counted', JSON.stringify(greetNo));
 
-    var name = inputName.value
-    var checkedRadioBtn = document.querySelector("input[name='language']:checked");
-    if(checkedRadioBtn != null && name != ""){
-
-      var selectedLanguage = checkedRadioBtn.value;
-      var greetMessage = greetMe.greetNeighbour(selectedLanguage, name);
-
-      var greetsNo = greetMe.checkGreets();
-      var greetedNamesObj = greetMe.getGreetedNames();
-
-      greetingElement.innerHTML = greetMessage;
-      localStorage.setItem('namesGreeted', JSON.stringify(greetedNamesObj));
-      greetCounterElem.innerHTML = greetsNo;
-      greetCounterElem.value = "";
-      localStorage.setItem('namesGreeted', JSON.stringify(greetedNamesObj));
-
-    }else{
-      inputName.value = "";
-      greetCounterElem.value= "";
-    }
-  }
-
-
-function clickTheClearButton(){
-  greetMe = Greetings();
-
-  var greetsNo = greetMe.countAllGreets();
-  var greetedNamesObj = greetMe.countAllNames();
-  localStorage.setItem('namesGreeted', JSON.stringify(greetedNamesObj));
-
-  inputName.innerHTML = "";
-  greetCounterElem.innerHTML= greetsNo;
-
+  //greetCounterElem.innerHTML = greetNo;
+  //localStorage.setItem('counted', JSON.stringify(greetNo));
 }
 
+function clickTheClearButton() {
+  greetingElement.innerHTML = '';
+  localStorage.setItem('greetedNames',JSON.stringify({}));
 
+  greetSpotter.countAllGreets();
 
+  greetCounterElem.innerHTML =   greetSpotter.countAllGreets();
+  //localStorage.clear();
+};
 
-
-  // var greetSpotter = Greetings(storedCounter);
-  // greetCounterElem.innerHTML = storedCounter;
-  //
-  // function clickGreetBtn() {
-  //   var name = inputName.value;
-  //   var checkedRadioBtn = document.querySelector("input[name='language']:checked");
-  //   if (checkedRadioBtn) {
-  //     var languageChoice = checkedRadioBtn.value;
-  //     var displayGreeting = greetSpotter.allGreetings(languageChoice, name);
-  //     localStorage.setItem('counted', greetSpotter.countAllgreets());
-  //     greetingElement.innerHTML = displayGreeting;
-  //     greetCounterElem.innerHTML = greetSpotter.countAllgreets();
-  //   }
-  // }
-  //
-  // function clickTheClearButton() {
-  //   localStorage.clear();
-  //   location.reload();
-  //   greetCounterElem.innerHTML = count;
-  //   // inputName.innerHTML = "";
-  //   // greetCounterElem.innerHTML = greetsNo;
-  //
-  // };
-
-  greetBtnElement.addEventListener('click', clickGreetBtn);
-  clearBtnElement.addEventListener('click', clickTheClearButton);
+greetBtnElement.addEventListener('click', clickGreetBtn);
+clearBtnElement.addEventListener('click', clickTheClearButton);
--- js/greet.js
+++ js/greet.js
@@ -1,28 +1,37 @@
 function Greetings(stored) {
-  var greetingSpotted = stored ? Object.keys(stored).length : 0;
-  var greetedNames = stored || {};
+  // console.log('stored : '+stored);
+  //var greetingSpotted = stored ? Object.keys(stored).length : 0;
+  var greetedNames = stored;
 
   function greet(language, name) {
+    console.log(name, language);
     if (name != '') {
       if (greetedNames[name] === undefined) {
         greetedNames[name] = 0;
+        localStorage.setItem('greetedNames',JSON.stringify(greetedNames));
+        console.log(greetedNames);
+
       }
-      if (language === "English") {
-        return "Hello, " + name;
+      if (language === 'English') {
+        return 'Hello, ' + name;
       }
-      if (language === "Afrikaans") {
-        return "Hallo, " + name;
+      if (language === 'Afrikaans') {
+        return 'Hallo, ' + name;
       }
       if (language === "isiXhosa") {
-        return "Molo, " + name;
+        return 'Molo, ' + name;
       }
 
     }
   }
 
   function greetSpottedCounter() {
-    return Object.keys(greetedNames).length;
-    // var counter = 0
+    var storedNames = JSON.parse(localStorage.getItem("greetedNames"));
+    console.log(storedNames);
+    // console.log('storedNames :'+ storedNames));
+    console.log(Object.keys(storedNames).length);
+     return Object.keys(storedNames).length;
+    // // var counter = 0
     // for (var name in greetedNames){
     //     counter++;
     // }
